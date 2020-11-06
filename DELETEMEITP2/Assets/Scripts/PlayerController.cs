6f998e4d9aa4f232fc431bf2852d2aa3758f7116
Mark Suter <msuter@live.elida.k12.oh.us>
2020-11-06 08:49:00 -0500

    added script

--- DELETEMEITP2/Assets/Scripts/PlayerController.cs
+++ DELETEMEITP2/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Mr. Suter was here");
     }
 
     // Update is called once per frame
