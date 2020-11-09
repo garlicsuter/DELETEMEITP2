548b4c5af5707ab71817e23f760bcbd718f1a35d
Mark Suter <msuter@live.elida.k12.oh.us>
2020-11-09 09:09:24 -0500

    all ldebugs

--- DELETEMEITP2/Assets/Scripts/PlayerController.cs
+++ DELETEMEITP2/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,10 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         Debug.Log("Mr. Suter was here 11/9/2020");
         Debug.Log("BJ was here");
         Debug.Log("Mr. Suter was here");
         Debug.Log("BV");
-        Debug.Log("Mr. Suter was here 11/9/2020");
         Debug.Log("MJ");
         Debug.Log("LA");
 
