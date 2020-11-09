f59bd6dabc923765135ceb868817b2bd9659a0ae
Maurice Jackson <21JackM@live.elida.k12.oh.us>
2020-11-09 08:53:37 -0500

    debug.log initials

--- DELETEMEITP2/Assets/Scripts/PlayerController.cs
+++ DELETEMEITP2/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mr. Suter was here");
+        Debug.Log("Mr. Suter was here 11/9/2020");
+        Debug.Log("MJ");
     }
 
     // Update is called once per frame
